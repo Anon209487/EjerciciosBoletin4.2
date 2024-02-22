d03df5068bfc626965bcd62249cc1bd7d34ae2a3
Anon209487 <156091669+Anon209487@users.noreply.github.com>
2024-02-22 20:02:54 +0100

    Update and rename Act3.java to Act4.java

---
+++ Act4.java
@@ -0,0 +1,9 @@
+	public static void main(String[] args) {
+		Scanner frase = new Scanner(System.in);
+		System.out.println("intrdocuce cadena");
+		String string =frase.nextLine();
+		String[]fraseordenada = string.split(" ");
+		Arrays.sort(fraseordenada);
+		System.out.println(Arrays.toString(fraseordenada));
+}
+}
