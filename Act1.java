ddf918351223efe2d2a634da4db18b1caefbd283
Anon209487 <156091669+Anon209487@users.noreply.github.com>
2024-02-22 20:00:15 +0100

    Update and rename Act1Boletin1.java to Act1.java

---
+++ Act1.java
@@ -0,0 +1,23 @@
+Scanner scanner = new Scanner(System.in);
+
+        System.out.println("introduce una caedena"); 
+         String cadena = scanner.nextLine(); 
+         System.out.println("introduce una caedena"); 
+         String cadena2 = scanner.nextLine(); 
+         int longitud;
+         int longitud2;
+     longitud = cadena.length(); 
+     longitud2 = cadena2.length(); 
+        if (longitud2>longitud){
+         System.out.print(cadena2);
+            System.out.println("larga"); 
+        }
+        if (longitud>longitud2){
+         System.out.print(cadena);
+          System.out.println("larga"); 
+        }
+        }
+       }  
+        
+
+
